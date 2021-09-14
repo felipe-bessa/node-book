848bf7f852531b7df960e85679279a0fee875683
FelipeBessa <felipe-bessa@outlook.com>
2021-09-14 00:10:34 -0300

    GET /products - Make the test pass

--- src/app.js
+++ src/app.js
@@ -5,5 +5,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/products', (req, res) => {
+    res.send([{
+        name: 'Default product',
+        description: 'product description',
+        price: 100
+    }]).sendStatus(200);
+});
 
 module.exports = app;
