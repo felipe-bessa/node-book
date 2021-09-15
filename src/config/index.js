e2459871764ee3d57bef18523c1d305b5ef6fd96
FelipeBessa <felipe-bessa@outlook.com>
2021-09-15 01:48:20 -0300

    Set environment path variables and configurations

---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = {
+    app: {
+        port: process.env.PORT || 4000,
+    }
+}
--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 'use strict'
+require('dotenv').config();
+
+const config = require('./config');
 
 const app = require ('./app.js');
 
-const port = 31912;
+//const port = 31912;
 
-app.listen(port, (err) => {
+app.listen(config.app.port, (err) => {
     if (err) return console.error('erro');
 
-    console.log(`Running at: http://localhost:${port}/`);
+    console.log(`Running at: http://localhost:${config.app.port}/`);
 });
